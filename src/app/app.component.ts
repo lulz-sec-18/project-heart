4b9047db1003e239db525f638bef7b7ab495adc2
ajay7231 <53226413+ajay7231@users.noreply.github.com>
2021-04-24 19:42:41 +0530

    navbar and about section done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'project-heart';
+
+  ngAfterViewInit() {
+     
+  }
+  navbarToggler(el): void {
+     el.classList.toggle('active')
+  }
 }
