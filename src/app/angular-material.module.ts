4bd5401571d683d0d8426bb036c5661a29157c98
ajay7231 <53226413+ajay7231@users.noreply.github.com>
2021-05-02 15:10:17 +0530

    footer

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule} from '@angular/material/divider';
 
-const materialModules = [MatIconModule,MatButtonModule];
+const materialModules = [MatIconModule,MatButtonModule,MatGridListModule,MatDividerModule];
 
 @NgModule({
   imports: [CommonModule, ...materialModules],
