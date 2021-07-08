731f30441771959130abeab6b9a495f6aa09ab1e
ajay7231 <53226413+ajay7231@users.noreply.github.com>
2021-07-08 18:07:02 +0530

    add-patient-issue

--- src/app/pages/dashboard/add-patient/add-patient.component.ts
+++ src/app/pages/dashboard/add-patient/add-patient.component.ts
@@ -1,11 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AuthService } from "src/app/services/auth.service";
-import { Patient } from "../../../models/patient.model";
-import { User } from "../../../models/user.model";
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Patient } from '../../../models/patient.model';
+import { User } from '../../../models/user.model';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { PatientAttributes } from '../../../models/patient-attributes.model';
 import { Router } from '@angular/router';
 import { PredictionService } from 'src/app/services/prediction.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-patient',
@@ -13,7 +20,7 @@ import { PredictionService } from 'src/app/services/prediction.service';
 })
 export class AddPatientComponent implements OnInit {
   patientForm: FormGroup;
-  patientAttributes:PatientAttributes;
+  patientAttributes: PatientAttributes;
   newPatient: Patient;
   currentUser: User;
   predictionResult;
@@ -22,95 +29,147 @@ export class AddPatientComponent implements OnInit {
     public authService: AuthService,
     private formBuilder: FormBuilder,
     private router: Router,
-    private predictionService: PredictionService
+    private predictionService: PredictionService,
+    private _snackBar: MatSnackBar
   ) {}
 
-  getMedicine(form:FormGroup) {
-    return form.get('medicine') as FormArray
+  getMedicine(form: FormGroup) {
+    return form.get('medicine') as FormArray;
   }
 
-  patientFormValueToString(controlName: string | (string | number)[],form:FormGroup) {
-    return form.get(controlName).value !== null ?
-           this.patientForm.get(controlName).value.toString() : null;
+  patientFormValueToString(
+    controlName: string | (string | number)[],
+    form: FormGroup
+  ) {
+    return form.get(controlName).value !== null
+      ? this.patientForm.get(controlName).value.toString()
+      : null;
   }
 
-  patientFormValueToInt(controlName: string | (string | number)[],form:FormGroup) {
-    return form.get(controlName).value !== null ?
-           parseInt(this.patientForm.get(controlName).value) : null;
+  patientFormValueToInt(
+    controlName: string | (string | number)[],
+    form: FormGroup
+  ) {
+    return form.get(controlName).value !== null
+      ? parseInt(this.patientForm.get(controlName).value)
+      : null;
   }
 
   ngOnInit(): void {
     this.currentUser = JSON.parse(localStorage.getItem('user'));
     this.patientForm = new FormGroup({
-      patientName: new FormControl(null, Validators.pattern(/^[a-z,',-]+(\s)[a-z,',-]+$/i)),
-      age: new FormControl(null,),
+      patientName: new FormControl(
+        null,
+        Validators.pattern(/^[a-z,',-]+(\s)[a-z,',-]+$/i)
+      ),
+      age: new FormControl(null, [Validators.min(0), Validators.max(120)]),
       gender: new FormControl(1),
       symptoms: new FormControl(null),
       disease: new FormControl(null),
       chestPainType: new FormControl(1),
-      restingBp: new FormControl(null),
-      cholesterol: new FormControl(null),
-      fastingBp: new FormControl(null),
+      restingBp: new FormControl(null, [
+        Validators.min(0),
+        Validators.max(500),
+      ]),
+      cholesterol: new FormControl(null, [
+        Validators.min(0),
+        Validators.max(1000),
+      ]),
+      fastingBp: new FormControl(null, [
+        Validators.min(0),
+        Validators.max(500),
+      ]),
       restingEcg: new FormControl(0),
-      maxHeartRate: new FormControl(null),
+      maxHeartRate: new FormControl(null, [
+        Validators.min(0),
+        Validators.max(500),
+      ]),
       exerciseInducedAngina: new FormControl(0),
-      exerciseInducedDepression: new FormControl(null),
+      exerciseInducedDepression: new FormControl(null, [
+        Validators.min(0),
+        Validators.max(500),
+      ]),
       slopeOfStSegment: new FormControl(2),
       majorVessels: new FormControl(null),
-      thalassemia: new FormControl(null),
+      thalassemia: new FormControl(null, [
+        Validators.min(0),
+        Validators.max(10),
+      ]),
       confirm: new FormControl(false),
       medicine: this.formBuilder.array([
         this.formBuilder.group({
           medicineName: [''],
           dose: ['', [Validators.min(1), Validators.max(10)]],
-          remarks: ['']
-        })
-      ])
-    })
+          remarks: [''],
+        }),
+      ]),
+    });
   }
+  // async calculateResult(patientDetails: PatientAttributes) {
+  //   await this.predictionService.predictResult(
+  //     Object.values(patientDetails)).then((value) => {
+  //       this.predictionResult = value[0];
+  //       console.log(value[0]);
+  //     });
+  // }
 
-  async calculateResult(patientDetails: PatientAttributes) {
-    await this.predictionService.predictResult(
-      Object.values(patientDetails)).then((value) => {
-        this.predictionResult = value[0];
-        console.log(value[0]);
-      });
-  }
-
-  onSubmit(form:FormGroup) {
+  async onSubmit(form: FormGroup) {
     this.patientAttributes = {
-      age: this.patientFormValueToInt('age',form),
-      gender: this.patientFormValueToInt('gender',form),
-      chestPainType: this.patientFormValueToInt('chestPainType',form),
-      restingBp: this.patientFormValueToInt('restingBp',form),
-      cholesterol: this.patientFormValueToInt('cholesterol',form),
-      fastingBp: this.patientFormValueToInt('fastingBp',form),
-      restingEcg: this.patientFormValueToInt('restingEcg',form),
-      maxHeartRate: this.patientFormValueToInt('maxHeartRate',form),
-      exerciseInducedAngina: this.patientFormValueToInt('exerciseInducedAngina',form),
-      exerciseInducedDepression: this.patientFormValueToInt('exerciseInducedDepression',form),
-      slopeOfStSegment: this.patientFormValueToInt('slopeOfStSegment',form),
-      majorVessels: this.patientFormValueToInt('majorVessels',form),
-      thalassemia: this.patientFormValueToInt('thalassemia',form)
-    }
+      age: this.patientFormValueToInt('age', form),
+      gender: this.patientFormValueToInt('gender', form),
+      chestPainType: this.patientFormValueToInt('chestPainType', form),
+      restingBp: this.patientFormValueToInt('restingBp', form),
+      cholesterol: this.patientFormValueToInt('cholesterol', form),
+      fastingBp: this.patientFormValueToInt('fastingBp', form),
+      restingEcg: this.patientFormValueToInt('restingEcg', form),
+      maxHeartRate: this.patientFormValueToInt('maxHeartRate', form),
+      exerciseInducedAngina: this.patientFormValueToInt(
+        'exerciseInducedAngina',
+        form
+      ),
+      exerciseInducedDepression: this.patientFormValueToInt(
+        'exerciseInducedDepression',
+        form
+      ),
+      slopeOfStSegment: this.patientFormValueToInt('slopeOfStSegment', form),
+      majorVessels: this.patientFormValueToInt('majorVessels', form),
+      thalassemia: this.patientFormValueToInt('thalassemia', form),
+    };
 
-    this.calculateResult(this.patientAttributes);
+    await this.predictionService.predictResult(
+      this.patientAttributes,
+      (result) => (this.predictionResult = result[0])
+    );
 
-    this.newPatient = this.currentUser == null ? null : {
-      doctor_uid: this.currentUser.uid,
-      admission_time: Date.now(),
-      name: this.patientFormValueToString('patientName',form),
-      gender: this.patientFormValueToInt('gender',form) == 1 ? "male" : "female",
-      id: null,
-      disease: this.patientFormValueToString('disease',form),
-      symptoms: this.patientFormValueToString('symptoms',form),
-      prescribedDose: this.getMedicine(form).value,
-      attributes: this.patientAttributes,
-      attributesArray: Object.values(this.patientAttributes),
-      condition: this.predictionResult
-    }
+    this.newPatient =
+      this.currentUser == null
+        ? null
+        : {
+            doctor_uid: this.currentUser.uid,
+            admission_time: Date.now(),
+            name: this.patientFormValueToString('patientName', form),
+            gender:
+              this.patientFormValueToInt('gender', form) == 1
+                ? 'male'
+                : 'female',
+            id: null,
+            disease: this.patientFormValueToString('disease', form),
+            symptoms: this.patientFormValueToString('symptoms', form),
+            prescribedDose: this.getMedicine(form).value,
+            attributes: this.patientAttributes,
+            attributesArray: Object.values(this.patientAttributes),
+            condition: this.predictionResult,
+          };
     console.log(this.newPatient);
-    this.authService.createPatient(this.newPatient).then((err)=>console.log(err));
-    this.router.navigate(['/dashboard/patient-list']);
+    this.authService.createPatient(this.newPatient).then((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this._snackBar.open('Patient Added Successfully', 'Success', {
+          duration: 2000,
+        });
+        this.router.navigate(['/dashboard/patient-list']);
+      }
+    });
   }
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -16,8 +16,10 @@ export class DashboardComponent implements OnInit {
     this.authService.firestoreInit();
 
     //testing purpose
-    this.predictor
-      .predictResult([44, 1, 2, 140, 235, 0, 0, 180, 0, 0, 2, 0, 2])
-      .then((value) => console.log(value[0] > 0.5));
+    // this.predictor
+    //   .predictResult([44, 1, 2, 140, 235, 0, 0, 180, 0, 0, 2, 0, 2], (value) => {
+    //     console.log(value);
+    //   })
+      
   }
 }
