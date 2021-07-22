494c959062ee59bec54f44267c1bbc29a3a9c6b8
ajay7231 <53226413+ajay7231@users.noreply.github.com>
2021-07-22 13:53:20 +0530

    prediction ui update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDrgaf0OGuyAYExHSvZk_cRKniE1x9MbS0',
+    authDomain: 'well-heart-ce9a5.firebaseapp.com',
+    databaseURL: 'https://well-heart-ce9a5-default-rtdb.firebaseio.com',
+    projectId: 'well-heart-ce9a5',
+    storageBucket: 'well-heart-ce9a5.appspot.com',
+    messagingSenderId: '681420795462',
+    appId: '1:681420795462:web:de43c86083a5114c47e447',
+    measurementId: 'G-ZDBZ9FJWQE',
+  },
 };
